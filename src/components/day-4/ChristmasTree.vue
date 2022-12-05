af7007f3d6c328b735e4cadbedade48a541ab907
K-shigehito <example@test.com>
2022-12-05 12:33:31 +0900

    feat: day-4

---
+++ src/components/day-4/ChristmasTree.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { computed } from "vue";
+
+type Props = {
+  size: "sm" | "md" | "lg";
+  volume: number;
+};
+const props = withDefaults(defineProps<Props>(), {
+  size: "md",
+  volume: 1,
+});
+
+const circleSize = computed(() => {
+  switch (props.size) {
+    case "sm":
+      return "h-12 w-12 -m-1.5";
+    case "md":
+      return "h-16 w-16 -m-2";
+    case "lg":
+      return "h-20 w-20 -m-3";
+  }
+});
+</script>
+
+<template>
+  <div>
+    <ChristmasTree :size="props.size" v-if="volume > 1" :volume="volume - 1" />
+
+    <div class="flex flex-row justify-center">
+      <div
+        v-for="i in volume"
+        class="relative flex items-center justify-center rounded-full bg-green-500"
+        :class="circleSize"
+      />
+    </div>
+  </div>
+</template>
