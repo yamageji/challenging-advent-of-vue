afc7eefdbaeea28c93bc46a418b1dc754cf4f0f6
K-shigehito <71963890+K-shigehito@users.noreply.github.com>
2022-12-11 21:48:55 +0900

    feat: day-9

---
+++ src/views/Day9View.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import TopHeading from "@/components/UI/TopHeading.vue";
+import PageDay9 from "@/components/day-9/PageDay9.vue";
+</script>
+
+<template>
+  <main>
+    <TopHeading>
+      <template v-slot:day> Day 9 </template>
+      i18n happy holidays
+    </TopHeading>
+    <div class="mt-10">
+      <PageDay9 />
+    </div>
+  </main>
+</template>
