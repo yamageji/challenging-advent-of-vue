d6b847a054380ae6a4f423615bd1224e11f15ef9
K-shigehito <example@test.com>
2022-12-12 12:41:47 +0900

    feat: day-11

---
+++ src/components/day-11/ChristmasLights.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+type Props = {
+  isTwincle: boolean;
+};
+const props = defineProps<Props>();
+
+const AMPLIFY = 15;
+const offsetY = Math.random() * AMPLIFY - AMPLIFY / 2;
+const offsetX = Math.random() * AMPLIFY - AMPLIFY / 2;
+const MAX_DELAY = 2;
+const twinkleDelay = Math.random() * MAX_DELAY;
+</script>
+
+<template>
+  <div
+    :class="{
+      'twinkle relative z-20 m-2 h-2 w-2 rounded-full bg-yellow-100 shadow-md':
+        props.isTwincle,
+    }"
+    :style="{
+      top: `${offsetY}px`,
+      left: `${offsetX}px`,
+      'animation-delay': `${twinkleDelay}s`,
+    }"
+  ></div>
+</template>
+
+<style scoped>
+.twinkle {
+  animation: twinkle 2s ease-in-out infinite;
+}
+@keyframes twinkle {
+  0% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+}
+</style>
---
+++ src/views/Day11View.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import TopHeading from "@/components/UI/TopHeading.vue";
+import PageDay11 from "@/components/day-11/PageDay11.vue";
+</script>
+
+<template>
+  <main>
+    <TopHeading>
+      <template v-slot:day> Day 11 </template>
+      Xmas Tree Lights
+    </TopHeading>
+    <div class="mt-10">
+      <PageDay11 />
+    </div>
+  </main>
+</template>
