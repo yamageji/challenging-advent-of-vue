95b0d71303eb7e6f5716046469cc6427e501d0b1
K-shigehito <example@test.com>
2022-12-13 17:29:22 +0900

    fix: say-12

--- src/components/day-12/PageDay12.vue
+++ src/components/day-12/PageDay12.vue
@@ -1,1 +1,19 @@
-<template>test</template>
+<script setup lang="ts">
+import { ref, computed } from "vue";
+const volume = ref(10);
+const boxList = computed(() => new Array(volume.value));
+</script>
+
+<template>
+  <input type="range" v-model="volume" />
+  {{ volume }}
+  <ul class="flex flex-wrap gap-2">
+    <li
+      v-for="(box, index) in boxList"
+      :key="index"
+      class="flex h-10 w-10 items-center justify-center rounded-md bg-stone-300"
+    >
+      {{ index }}
+    </li>
+  </ul>
+</template>
