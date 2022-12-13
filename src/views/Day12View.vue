77ffcae2c315d8010f51d61a2ab560f2aa368cf3
K-shigehito <example@test.com>
2022-12-13 16:43:48 +0900

    feat: day-12

---
+++ src/views/Day12View.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import TopHeading from "@/components/UI/TopHeading.vue";
+import PageDay12 from "@/components/day-12/PageDay12.vue";
+</script>
+
+<template>
+  <main>
+    <TopHeading>
+      <template v-slot:day> Day 12 </template>
+      find josephs gift
+    </TopHeading>
+    <div class="mt-10">
+      <PageDay12 />
+    </div>
+  </main>
+</template>
