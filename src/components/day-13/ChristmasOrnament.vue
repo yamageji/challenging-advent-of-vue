bda125e5da38b2f4ff4da91287fe10a216466313
K-shigehito <71963890+K-shigehito@users.noreply.github.com>
2022-12-14 22:50:31 +0900

    fix: day-13

---
+++ src/components/day-13/ChristmasOrnament.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const AMPLIFY = 14;
+const offsetY = 16 + (Math.random() * AMPLIFY - AMPLIFY / 2);
+const offsetX = 16 + (Math.random() * AMPLIFY - AMPLIFY / 2);
+withDefaults(
+  defineProps<{
+    ornament: boolean;
+  }>(),
+  {
+    ornament: false,
+  }
+);
+
+const randomColor = (isShow: boolean) => {
+  if (!isShow) return "";
+  const selectColor = Math.floor(Math.random() * 3);
+  if (selectColor === 0) {
+    return "bg-sky-500";
+  } else if (selectColor === 1) {
+    return "bg-rose-400";
+  }
+};
+</script>
+
+<template>
+  <div
+    class="absolute z-10 h-4 w-4 rounded-full"
+    :class="randomColor(ornament)"
+    :style="{
+      top: `${offsetY}px`,
+      left: `${offsetX}px`,
+    }"
+  ></div>
+</template>
--- src/components/day-13/PageDay13.vue
+++ src/components/day-13/PageDay13.vue
@@ -2,13 +2,14 @@
 import { ref } from "vue";
 import ChristmasTree from "./ChristmasTree.vue";
 import ChristmasLights from "./ChristmasLights.vue";
-import ChristmasOnament from "./ChristmasOnament.vue";
+import ChristmasOrnament from "./ChristmasOrnament.vue";
 import BaseButton from "../UI/BaseButton.vue";
 import { Switch } from "@headlessui/vue";
 
 const MAX_VOLUME = 5;
 const volume = ref(1);
-const isTwincleOn = ref(false);
+const isTwinkleOn = ref(false);
+const isOrnament = ref(false);
 </script>
 
 <template>
@@ -39,14 +40,32 @@ const isTwincleOn = ref(false);
       >
         <p class="text-lg font-bold text-green-700">light switch</p>
         <Switch
-          v-model="isTwincleOn"
-          :class="isTwincleOn ? 'bg-green-700' : 'bg-stone-500'"
+          v-model="isTwinkleOn"
+          :class="isTwinkleOn ? 'bg-green-700' : 'bg-stone-500'"
           class="relative inline-flex h-8 w-16 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent px-1 transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-stone-900 focus-visible:ring-opacity-75"
         >
           <span class="sr-only">Use setting</span>
           <span
             aria-hidden="true"
-            :class="isTwincleOn ? 'translate-x-7' : 'translate-x-0'"
+            :class="isTwinkleOn ? 'translate-x-7' : 'translate-x-0'"
+            class="pointer-events-none inline-block h-6 w-6 transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out"
+          />
+        </Switch>
+      </div>
+
+      <div
+        class="mt-4 flex flex-col items-center gap-3 rounded-md border border-stone-300 px-4 py-2"
+      >
+        <p class="text-lg font-bold text-green-700">ornament switch</p>
+        <Switch
+          v-model="isOrnament"
+          :class="isOrnament ? 'bg-green-700' : 'bg-stone-500'"
+          class="relative inline-flex h-8 w-16 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent px-1 transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-stone-900 focus-visible:ring-opacity-75"
+        >
+          <span class="sr-only">Use setting</span>
+          <span
+            aria-hidden="true"
+            :class="isOrnament ? 'translate-x-7' : 'translate-x-0'"
             class="pointer-events-none inline-block h-6 w-6 transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out"
           />
         </Switch>
@@ -56,8 +75,8 @@ const isTwincleOn = ref(false);
     <div class="mt-4 flex flex-col items-center justify-center">
       <div class="flex h-full w-full items-center justify-center">
         <ChristmasTree size="sm" :volume="volume">
-          <ChristmasLights :is-twinkle="isTwincleOn" />
-          <ChristmasOnament />
+          <ChristmasLights :is-twinkle="isTwinkleOn" />
+          <ChristmasOrnament :ornament="isOrnament" />
         </ChristmasTree>
       </div>
       <div class="-m-2 h-12 w-6 rounded-sm bg-amber-700"></div>
